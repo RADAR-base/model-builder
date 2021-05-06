6037c7316807c8279cec1f349fbff941b9344e51
Heet Sankesara <heetsankesara3@gmail.com>
2021-05-06 16:22:07 +0100

    Added lstm encoder and ran it.

--- model-builder/ml_models/lung_study_lstm_anomaly_detection/lstm.py
+++ model-builder/ml_models/lung_study_lstm_anomaly_detection/lstm.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 import mlflow
+import mlflow.pyfunc
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
 class LSTM(nn.Module):
     # input_dim has to be size after flattening
@@ -13,37 +16,57 @@ class LSTM(nn.Module):
         num_layers: int,
     ):
         super(LSTM, self).__init__()
-        self.input_dimensionality: int = input_dimensionality
-        self.input_dim: int = input_dim  # It is 1d, remember
+        self.input_feature_len = input_dimensionality
+        self.input_dim: int = input_dim
         self.latent_dim: int = latent_dim
         self.num_layers: int = num_layers
-        self.encoder = torch.nn.LSTM(self.input_dim, self.latent_dim, self.num_layers)
-        # You can have any latent dim you want, just output has to be exact same size as input
-        # In this case, only encoder and decoder, it has to be input_dim though
-        self.decoder = torch.nn.LSTM(self.latent_dim, self.input_dim, self.num_layers)
+        self.encoder = torch.nn.LSTM(self.input_feature_len, self.latent_dim, self.num_layers)
+        self.decoder = torch.nn.LSTM(self.latent_dim, self.input_feature_len, self.num_layers)
+
+        nn.init.xavier_uniform(self.encoder.weight_ih_l0, gain=np.sqrt(2))
+        nn.init.xavier_uniform(self.decoder.weight_ih_l0, gain=np.sqrt(2))
 
     def forward(self, input):
-        # Save original size first:
-        original_shape = input.shape
+        input = input.float()
         # Flatten 2d (or 3d or however many you specified in constructor)
-        input = input.reshape(input.sh  ape[: -self.input_dimensionality] + (-1,))
+        #input = input.reshape(input.shape[: -self.input_dimensionality] + (-1,))
 
         # Rest goes as in my previous answer
-        _, (last_hidden, _) = self.encoder(input)
-        encoded = last_hidden.repeat(input.shape)
+        encoded, _ = self.encoder(input)
         y, _ = self.decoder(encoded)
 
-        # You have to reshape output to what the original was
-        reshaped_y = y.reshape(original_shape)
-        return torch.squeeze(reshaped_y)
+        return y
 
-class MnistTorchRNN(mlflow.pyfunc.PythonModel):
+class LSTMWrapper(mlflow.pyfunc.PythonModel):
 
-    def load_context(self, context):
-        self.model = mlflow.pytorch.load_model(
-            context.artifacts["torch-rnn-model"], map_location="cpu")
-        self.model.to('cpu')
+    def __init__(self, model, threshold):
+          self.model = model
+          self.threshold = threshold
+
+    def _get_device(self):
+        if torch.cuda.is_available():
+            return "gpu"
+        else:
+            return "cpu"
+
+    def _predict(self, dataloader):
+        device = self._get_device()
         self.model.eval()
+        criterion = nn.L1Loss(reduction='sum').to(device)
+        predictions  = []
+        for seq_true in dataloader:
+            seq_true = seq_true.to(device)
+            seq_pred = self.model(seq_true)
+            loss = criterion(seq_pred, seq_true)
+            predictions.append((loss < self.threshold).tolist())
+        return predictions
+
+
+    def predict(self, context, model_input):
+        raw_data, raw_data_index = model_input[0], model_input[1]
+        prediction_dataset = LungStudyDataset(raw_data, raw_data_index)
+        prediction_dataloader = DataLoader(prediction_dataset, batch_size=8)
+        return self._predict(prediction_dataloader).tolist()
 
     def predict(self, context, input_df):
         import numpy as np
@@ -52,3 +75,14 @@ class MnistTorchRNN(mlflow.pyfunc.PythonModel):
                 input_df.values.reshape(-1, 28, 28).astype(np.float32)).to('cpu')
             model_results = self.model(input_tensor).numpy()
             return np.power(np.e, model_results)
+
+class LungStudyDataset(Dataset):
+    def __init__(self, data, data_index):
+        self.data = data
+        self.data_index = data_index
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
---
+++ model-builder/ml_models/lung_study_lstm_anomaly_detection/train.py
@@ -0,0 +1,168 @@
+import os
+import warnings
+import sys
+import argparse
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import IsolationForest
+from mlflow.tracking import MlflowClient
+# Get the current working directory
+sys.path.append(os.path.abspath('model-builder/'))
+from dataloader.postgres_pandas_wrapper import PostgresPandasWrapper
+from dataloader.querybuilder import QueryBuilder
+sys.path.append(os.path.abspath('.'))
+from model_class.lung_study import  LungStudy
+from lstm import LSTM, LSTMWrapper, LungStudyDataset
+
+from torch.utils.data import Subset
+from sklearn.model_selection import train_test_split
+import mlflow
+import mlflow.pytorch
+from torch.utils.data import DataLoader
+import torch
+from torch import nn
+import time
+from utils import fig2data, result_plot
+
+import mlflow.pyfunc
+
+def set_env_vars():
+    os.environ["MLFLOW_URL"] = "http://127.0.0.1:5000"
+    os.environ["MLFLOW_TRACKING_URI"] = "http://127.0.0.1:5000"
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://127.0.0.1:9000"
+    os.environ["AWS_ACCESS_KEY_ID"] = ""
+    os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+
+
+def log_scalar(name, value, step):
+    """Log a scalar value to MLflow"""
+    mlflow.log_metric(name, value, step=step)
+
+def train_model( train_dataset, val_dataset, model, device, n_epochs, lr):
+    model = model.to(device)
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    criterion = nn.L1Loss(reduction='sum').to(device)
+    history = dict(train_loss=[], val_loss=[])
+    for epoch in range(1 , n_epochs + 1):
+        model = model.train()
+        ts = time.time()
+        train_losses = []
+
+        for seq_true in train_dataset:
+            optimizer.zero_grad()
+
+            seq_true = seq_true.to(device)
+            seq_pred = model(seq_true)
+            loss = criterion(seq_pred, seq_true)
+            loss.backward()
+            optimizer.step()
+
+            train_losses.append(loss.item())
+
+        val_losses = []
+        model = model.eval()
+        with torch.no_grad():
+            for seq_true in val_dataset:
+
+                seq_true = seq_true.to(device)
+                seq_pred = model(seq_true)
+
+                loss = criterion(seq_pred, seq_true)
+
+                val_losses.append(loss.item())
+
+        te = time.time()
+        train_loss = np.mean(train_losses)
+        val_loss = np.mean(val_losses)
+        log_scalar("training_loss", train_loss, step=epoch)
+        mlflow.log_metric("validation_loss", val_loss, step=epoch)
+        mlflow.log_metric("test", epoch * 4, step=epoch)
+        history['train_loss'].append(train_loss)
+        history['val_loss'].append(val_loss)
+
+        print(f"Epoch: {epoch}  train loss: {train_loss}  val loss: {val_loss}  time: {te-ts} ")
+
+    return val_loss, history
+
+def train_val_dataset(dataset, dataset_index, val_split=0.25):
+    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    train_dataset = Subset(dataset, train_idx)
+    val_dataset = Subset(dataset, val_idx)
+    train_dataset_idx = Subset(dataset_index, train_idx)
+    val_dataset_idx = Subset(dataset_index, val_idx)
+    return LungStudyDataset(train_dataset, train_dataset_idx), LungStudyDataset(val_dataset, val_dataset_idx)
+
+def get_device():
+    if torch.cuda.is_available():
+        return "gpu"
+    else:
+        return "cpu"
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(42)
+
+    set_env_vars()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_layers", default=5)
+    parser.add_argument("--latent_dim", default=256)
+    parser.add_argument("--epochs", default=8)
+    parser.add_argument("--batch_size", default=8)
+    parser.add_argument("--learning_rate", default=0.01)
+    args = parser.parse_args()
+
+    lstm_conda_env={'channels': ['defaults'],
+     'name':'lstm_conda_env',
+     'dependencies': [ 'python=3.8', 'pip',
+     {'pip':['mlflow','pytorch','cloudpickle','pandas','numpy', 'torchvision']}]}
+
+    # Importing query builder
+    querybuilder = QueryBuilder(tablename="")
+    # Read the wine-quality data from the Postgres database using dataloader
+    # ADD DATABASE DETAILS HERE
+    lung_study = LungStudy()
+    dbconn = PostgresPandasWrapper()
+    dbconn.connect()
+    #print(dbconn.get_response(cols=["*"], dataset="wine_dataset"))
+    query = lung_study.get_query_for_training()
+    data = dbconn.get_response(query)
+    dbconn.disconnect()
+
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    #Set experiment
+    mlflow.set_experiment("lung_study_lstm_anomaly_detection")
+
+    dataset, dataset_index = lung_study.preprocess_data(data)
+
+    num_layers = args.num_layers
+    latent_dim = args.latent_dim
+    input_dim = dataset.shape[1]
+    input_dimensionality = dataset.shape[2]
+    epochs = args.epochs
+    batch_size = args.batch_size
+    lr = args.learning_rate
+    device =  get_device()
+    train_dataset, val_dataset = train_val_dataset(dataset, dataset_index)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+
+    with mlflow.start_run() as run:
+        mlflow.log_param("num_layers", num_layers)
+        mlflow.log_param("latent_dim", latent_dim)
+        mlflow.log_param("input_dim", input_dim)
+        mlflow.log_param("input_dimensionality", input_dimensionality)
+        mlflow.log_param("epochs", epochs)
+        mlflow.log_param("lr", lr)
+        mlflow.log_param("device", device)
+
+        lstm = LSTM(input_dimensionality, input_dim, latent_dim, num_layers)
+        threshold, history = train_model( train_dataloader, val_dataloader, lstm, device, epochs, lr)
+        mlflow.log_image(fig2data(result_plot(history)), "result_plot.png")
+        mlflow.log_param("Estimated Threshold", threshold)
+
+        mlflow.pyfunc.log_model(artifact_path="lung_study_lstm_anomaly_detection", python_model=LSTMWrapper(model=lstm, threshold=threshold), conda_env=lstm_conda_env, code_path=["model-builder/ml_models/lung_study_lstm_anomaly_detection/lstm.py"])
